c8f7757f3b2228900fe19da1a2f8c4e37d48dde1
JohnnyPea <JohnnyPea@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-14 23:09:39 +0000

    git-svn-id: https://plugins.svn.wordpress.org/check-last-login/trunk@311450 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- check-last-login.php
+++ check-last-login.php
@@ -3,24 +3,69 @@
 Plugin Name: Check Last Login
 Plugin URI: http://www.techforum.sk/
 Description: Checks user's login status
-Version: 0.2
+Version: 0.3
 Author: Ján Bočínec
 Author URI: http://johnnypea.wp.sk/
 License: GPL2
 */
 
-register_activation_hook(__FILE__, 'cron_activation');
-register_deactivation_hook(__FILE__, 'cron_deactivation');
+
+function check_last_login_menu() {
+  	add_submenu_page('options-general.php', 'Check Last Login', 'Check Last Login', 'manage_options', 'check-last-login', 'check_last_login_page' );
+
+	//call register settings function
+	add_action( 'admin_init', 'register_cll_settings' );
+}
+add_action('admin_menu', 'check_last_login_menu');
+
+function register_cll_settings() {
+	//register settings
+	register_setting( 'cll-group', 'allow_deletion' );
+	register_setting( 'cll-group', 'inactive_days' );
+
+}
+
+function check_last_login_page() { ?>
+		<div class="wrap"><h1>Check Last Login Settings</h1>
+		<form method="post" action="options.php">
+		<?php settings_fields( 'cll-group' ); ?>
+		<table class="form-table">		
+		<tr>
+		<td width="30%" valign="middle"><strong>Allow the automatic user deletion</strong></td>
+		<td width="70%"><input type="checkbox" name="allow_deletion" value="1" <?php if ( get_option('allow_deletion') ) echo 'checked="checked"'; ?> /></td>
+		</tr>
+		<?php
+		if ( get_option('allow_deletion') ) {
+			?>
+			<tr>
+			<td width="30%" valign="middle"><strong>Number of inactive days after the registration</strong></td>
+			<td width="70%"><input type="text" size="1" name="inactive_days" value="<?php if ( get_option('inactive_days') ) { echo get_option('inactive_days');}else{ echo 30; } ?>" /></td>
+			</tr>	
+			<?php
+		}
+		?>
+		</table>
+		<p class="submit">
+	    <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+	    </p>	
+		</form></div>
+<?php }
+
 
 add_action('cron_daily_event', 'is_user_active');
 
-function cron_activation() {
-	wp_schedule_event(time(), 'daily', 'cron_daily_event');
+if ( !wp_next_scheduled('my_task_hook') && get_option('allow_deletion') ) {
+	wp_schedule_event( time(), 'daily', 'cron_daily_event' );
+}
+
+if ( wp_next_scheduled('my_task_hook') && !get_option('allow_deletion') ) {
+	wp_clear_scheduled_hook('cron_daily_event');
 }
 
 function cron_deactivation() {
 	wp_clear_scheduled_hook('cron_daily_event');
 }
+register_deactivation_hook(__FILE__, 'cron_deactivation');
 
 function registration_login($user_ID) {
 	update_usermeta( $user_ID, 'last_user_login', 'No login' );
@@ -34,8 +79,9 @@ function last_user_login($login) {
 add_action('wp_login','last_user_login');
 
 function is_user_active() {
-	global $wpdb;	
-	$last_user_login = $wpdb->get_results("SELECT * FROM $wpdb->users INNER JOIN $wpdb->usermeta ON ID = user_id WHERE meta_key  = 'last_user_login' AND meta_value = 'No login' AND DATEDIFF(NOW(),user_registered) >= 30", ARRAY_A);
+	global $wpdb;
+	$inactive_days = get_option('inactive_days');
+	$last_user_login = $wpdb->get_results("SELECT * FROM $wpdb->users INNER JOIN $wpdb->usermeta ON ID = user_id WHERE meta_key  = 'last_user_login' AND meta_value = 'No login' AND DATEDIFF(NOW(),user_registered) >= $inactive_days", ARRAY_A);
 	
 	if ( $last_user_login )
 		foreach ( $last_user_login as $user_login )
--- readme.txt
+++ readme.txt
@@ -4,18 +4,21 @@ Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_i
 Tags: registration,login,user
 Requires at least: 2.8
 Tested up to: 3.0.1
-Stable tag: 0.2
+Stable tag: 0.3
 
 Simple plugin which checks user's login status and displays registration date and last login date columns on the "Users" page.
 
 == Description ==
 
-This plugin checks whether user logs in within 30 days after the registration if he does not his account is deleted. It also adds registration date and last login date columns on the "Users" page.
-
+This plugin adds registration date and last login date columns on the "Users" page.
+It also checks whether user logs in within 30 days after the registration if he does not his account is deleted - this option is disabled by default, you can change this settings on Check Last Login Settings page in Settings menu.
+ 
 **I am open to suggestions to improve the plugin !**
 
 == Installation ==
 Activate the plugin.
+
+You can enable/disable the automatic user deletion and set the inactivity time after the registration on Check Last Login Settings page in Settings menu.
 
 == Frequently Asked Questions ==
 
@@ -39,10 +42,16 @@ initial release
 = 0.2 =
 tiny fix to display "No login" if user has no log in from the time of plugin activation
 
+= 0.3 =
+added settings page - now you can enable/disable the automatic user deletion and set the inactivity time after the registration
+
 == Upgrade Notice ==
 
 = 0.1 =
 initial release
 
 = 0.2 =
-tiny fix to display "No login" if user has no log in from the time of plugin activation
+just update
+
+= 0.3 =
+just update
